9774925cebb1edf936d9c4f75f0e3e44c0d05fbc
PUGE <my@owo.help>
2017-01-18 11:05:52 +0800

    修复截图闪动

--- main.js
+++ main.js
@@ -1,94 +1,100 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const dialog = electron.dialog;
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null,
-  subWindow = null,
-  fs = require('fs'),
-  globalShortcut = require('electron').globalShortcut,
-  clipboard = require('electron').clipboard,
-  ipc = require('electron').ipcMain;
+const electron = require('electron'),
+      GlobalShortcut = require('electron').globalShortcut,
+      Clipboard = require('electron').clipboard,
+      BrowserWindow = electron.BrowserWindow,
+      Dialog = require('electron').dialog,
+      Ipc = require('electron').ipcMain,
+      App = electron.app,
+      Fs = require('fs');
+      
+let   mainWindow = null,subWindow = null;
   
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
+// 当所有窗口关闭时 结束进程.
+App.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    App.quit();
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
+// 程序成功启动事件
+App.on('ready', function() {
+  // 创建操作窗口
   mainWindow = new BrowserWindow({width: 92, height: 36, 
-    resizable: false, 
+    resizable: false, //不能改变窗口大小
     frame: false,
-    alwaysOnTop: true,
+    alwaysOnTop: true,//始终在最前端
     //fullscreen: true,
-    skipTaskbar: true
+    skipTaskbar: true//是否在任务栏中显示窗口
   });
   
-  //add shortcut
-  globalShortcut.register('ctrl+shift+q', function () {
+  //注册快捷键
+  //取消截图
+  GlobalShortcut.register('ctrl+shift+q', function () {
     mainWindow.webContents.send('global-shortcut-quit', 1);
   });
-  globalShortcut.register('ctrl+shift+c', function () {
+  //开始截图
+  GlobalShortcut.register('ctrl+shift+c', function () {
     mainWindow.webContents.send('global-shortcut-capture', 1);
   });
   
   
-  // and load the index.html of the app.
+  // 加载软件主页面.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
   
-  ipc.on('close', function () {
-    app.quit()
-  })
+  Ipc.on('close', function () {
+    App.quit();
+  });
   
-  ipc.on('create-sub-window', function (e, wh) {
-    subWindow = new BrowserWindow({width: wh[0], height: wh[1], fullscreen: true, resizable: false, skipTaskbar: true, frame: false, alwaysOnTop: true})
+  Ipc.on('create-sub-window', function (e, wh) {
+    mainWindow.hide();
+    subWindow = new BrowserWindow({
+      width: wh[0], 
+      height: wh[1], 
+      resizable: false, //不可改变大小
+      skipTaskbar: true, //无任务栏图标
+      frame: false, //
+      alwaysOnTop: true,//总是在最前
+      hasShadow:false,
+      transparent:true,
+      show:false
+    });
     //subWindow.webContents.openDevTools()
-    subWindow.loadURL('file://' + __dirname + '/sub.html')
-    mainWindow.hide()
-  })
+    let webContents = subWindow.webContents;
+    subWindow.loadURL('file://' + __dirname + '/sub.html');
+    webContents.on('did-finish-load', (event) => {
+      subWindow.show();
+    });
+  });
   
-  ipc.on('close-subwindow', function () {
-    subWindow.close()
-    mainWindow.show()
-  })
+  Ipc.on('close-subwindow', function () {
+    subWindow.close();
+    mainWindow.show();
+  });
   
-  ipc.on('cut', function (e, arg) {
+  Ipc.on('cut', function (e, arg) {
     subWindow.capturePage(arg, function (image) {
-      clipboard.writeImage(image)
+      Clipboard.writeImage(image)
       subWindow.close()
       mainWindow.show()
     })
   })
   
-  ipc.on('save-to-fs', function (e, arg) {
+  Ipc.on('save-to-fs', function (e, arg) {
     subWindow.capturePage(arg, function (image) {
       subWindow.setAlwaysOnTop(false)
-      dialog.showSaveDialog({title: '请选择保存路径', defaultPath: 'E:/', filters: [
+      Dialog.showSaveDialog({title: '请选择保存路径', defaultPath: 'E:/', filters: [
         { name: 'Images', extensions: ['png'] }
       ]}, function (p) {
-        fs.writeFile(p, image.toPng(), function () {
+        Fs.writeFile(p, image.toPng(), function () {
           subWindow.close()
           mainWindow.show()
         })
